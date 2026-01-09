0f7073f872550ec3447ddf2760815986d981538e
Loc Ta Duc <loctad@nashtechglobal.com>
2026-01-09 09:31:43 +0700

    remove scripts folder

--- scripts/sub.sh
+++
@@ -1,9 +0,0 @@
-# Get your subscription ID (it's already visible: 51e4dde5-a3cf-4369-b57f-137b90f633f0)
-SUBSCRIPTION_ID="51e4dde5-a3cf-4369-b57f-137b90f633f0"
-
-# Create Service Principal
-az ad sp create-for-rbac \
-  --name "Dynatrace-Monitoring-Zenfolio" \
-  --role "Monitoring Reader" \
-  --scopes "/subscriptions/$SUBSCRIPTION_ID" \
-  --json-auth
